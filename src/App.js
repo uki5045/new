4073df0e1a3af47d743d5b3e684eb29ffded12b6
hanjinwook <uki5045@gmail.com>
2022-03-27 15:40:30 +0900

    add header & footer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {Home, Login, Mypage, Signup, Notfound } from "./screens";
+import {Footer, Header} from "./components";
+import { Container } from "react-bootstrap";
 
 const App = () => {
     return (
         <BrowserRouter>
+            <Header />
           <Routes>
-            <Route path={'/'} element={<Home />} />
-            <Route path={'/login'} element={<Login />} />
-            <Route path={'/mypage'} element={<Mypage />} />
-            <Route path={'/signup'} element={<Signup />} />
-            <Route path={'*'} element={<Notfound />} />
+              <Route path={'/'} element={<Home />} />
+              <Route path={'/login'} element={<Login />} />
+              <Route path={'/mypage'} element={<Mypage />} />
+              <Route path={'/signup'} element={<Signup />} />
+              <Route path={'*'} element={<Notfound />} />
           </Routes>
+            <Footer />
         </BrowserRouter>
+
     );
 };
 
