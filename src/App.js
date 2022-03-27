fe50f24433f6bedd9a58436ae73ad4f2bb06a1eb
hanjinwook <uki5045@gmail.com>
2022-03-27 15:21:33 +0900

    project init & react router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Home, Login, Mypage, Signup, Notfound } from "./screens";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <BrowserRouter>
+          <Routes>
+            <Route path={'/'} element={<Home />} />
+            <Route path={'/login'} element={<Login />} />
+            <Route path={'/mypage'} element={<Mypage />} />
+            <Route path={'/signup'} element={<Signup />} />
+            <Route path={'*'} element={<Notfound />} />
+          </Routes>
+        </BrowserRouter>
+    );
+};
 
 export default App;
---
+++ src/screens/index.js
@@ -0,0 +1,7 @@
+import Home from "./Home";
+import Login from "./Login";
+import Mypage from "./Mypage";
+import Signup from "./Signup";
+import Notfound from "./Notfound";
+
+export { Home, Login, Mypage, Signup, Notfound }
